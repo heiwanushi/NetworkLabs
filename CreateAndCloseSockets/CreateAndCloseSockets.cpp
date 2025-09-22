593b3c3c25b49286969567077d0baf118d62e9d6
user <heiwanushi@gmail.com>
2025-09-23 00:32:25 +0300

    Reapply "sockets"
    This reverts commit 52464b3a2241999ebc000fe75da96ef75b05278f.

---
+++ CreateAndCloseSockets/CreateAndCloseSockets.cpp
@@ -0,0 +1,107 @@
+﻿// Программа для создания и закрытия сокетов TCP, IPX, SPX с выводом статуса работы каждого протокола
+// Русские комментарии сохранены
+
+#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <wsipx.h>  // Для IPX/SPX
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    int result;
+    bool tcp_ok = false, ipx_ok = false, spx_ok = false;
+
+    // --- Инициализация среды WinSock ---
+    result = WSAStartup(MAKEWORD(2, 0), &wsaData);
+    if (result != 0) {
+        std::cout << "WSAStartup(...) error: " << WSAGetLastError() << std::endl;
+        return 1;
+    }
+
+    // --- Создание сокета TCP ---
+    SOCKET s_tcp = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (s_tcp == INVALID_SOCKET) {
+        std::cout << "socket(...) for TCP error: " << WSAGetLastError() << std::endl;
+    }
+    else {
+        // --- bind() для TCP ---
+        sockaddr_in sa_tcp;
+        unsigned short tcp_port = 2000;
+        sa_tcp.sin_family = AF_INET;
+        sa_tcp.sin_port = htons(tcp_port);
+        sa_tcp.sin_addr.s_addr = htonl(INADDR_ANY);
+        memset(sa_tcp.sin_zero, 0, sizeof(sa_tcp.sin_zero));
+        result = bind(s_tcp, (sockaddr*)&sa_tcp, sizeof(sa_tcp));
+        if (result == SOCKET_ERROR) {
+            std::cout << "bind(...) for TCP error: " << WSAGetLastError() << std::endl;
+            closesocket(s_tcp);
+        }
+        else {
+            tcp_ok = true;
+        }
+    }
+
+    // --- Создание сокета IPX ---
+    SOCKET s_ipx = socket(AF_IPX, SOCK_DGRAM, NSPROTO_IPX);
+    if (s_ipx == INVALID_SOCKET) {
+        std::cout << "socket(...) for IPX error: " << WSAGetLastError() << std::endl;
+    }
+    else {
+        // --- bind() для IPX ---
+        sockaddr_ipx sa_ipx;
+        unsigned short ipx_socketnum = 2000;
+        memset(&sa_ipx, 0, sizeof(sa_ipx));
+        sa_ipx.sa_family = AF_IPX;
+        sa_ipx.sa_socket = ipx_socketnum;
+        result = bind(s_ipx, (sockaddr*)&sa_ipx, sizeof(sa_ipx));
+        if (result == SOCKET_ERROR) {
+            std::cout << "bind(...) for IPX error: " << WSAGetLastError() << std::endl;
+            closesocket(s_ipx);
+        }
+        else {
+            ipx_ok = true;
+        }
+    }
+
+    // --- Создание сокета SPX ---
+    SOCKET s_spx = socket(AF_IPX, SOCK_STREAM, NSPROTO_SPX);
+    if (s_spx == INVALID_SOCKET) {
+        std::cout << "socket(...) for SPX error: " << WSAGetLastError() << std::endl;
+    }
+    else {
+        // --- bind() для SPX ---
+        sockaddr_ipx sa_spx;
+        unsigned short spx_socketnum = 2000;
+        memset(&sa_spx, 0, sizeof(sa_spx));
+        sa_spx.sa_family = AF_IPX;
+        sa_spx.sa_socket = spx_socketnum;
+        result = bind(s_spx, (sockaddr*)&sa_spx, sizeof(sa_spx));
+        if (result == SOCKET_ERROR) {
+            std::cout << "bind(...) for SPX error: " << WSAGetLastError() << std::endl;
+            closesocket(s_spx);
+        }
+        else {
+            spx_ok = true;
+        }
+    }
+
+    // --- Закрытие сокетов ---
+    if (s_tcp != INVALID_SOCKET) closesocket(s_tcp);
+    if (s_ipx != INVALID_SOCKET) closesocket(s_ipx);
+    if (s_spx != INVALID_SOCKET) closesocket(s_spx);
+
+    // --- Завершить работу с WinSock ---
+    if (WSACleanup() == SOCKET_ERROR) {
+        std::cout << "WSACleanup() error: " << WSAGetLastError() << std::endl;
+    }
+
+    // --- Итог проверки ---
+    std::cout << std::endl << "*** Socket status ***" << std::endl;
+    std::cout << "TCP: " << (tcp_ok ? "Works correctly" : "Does NOT work") << std::endl;
+    std::cout << "IPX: " << (ipx_ok ? "Works correctly" : "Does NOT work") << std::endl;
+    std::cout << "SPX: " << (spx_ok ? "Works correctly" : "Does NOT work") << std::endl;
+
+    std::cout << "Program finished." << std::endl;
+    return 0;
+}
---
+++ UDPServer/UDPServer.cpp
@@ -0,0 +1,71 @@
+﻿#include <iostream>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#pragma comment(lib, "ws2_32.lib")
+
+int main() {
+    WSADATA wsaData;
+    SOCKET sock = INVALID_SOCKET;
+    sockaddr_in serverAddr, clientAddr;
+    int port;
+    char recvbuf[512];
+    int clientAddrLen = sizeof(clientAddr);
+
+    // Инициализация WinSock
+    int wsaResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (wsaResult != 0) {
+        std::cerr << "WSAStartup failed. Error code: " << wsaResult << std::endl;
+        return 1;
+    }
+
+    std::cout << "Enter port for server: ";
+    std::cin >> port;
+
+    // Создание сокета
+    sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed. Error code: " << WSAGetLastError() << std::endl;
+        WSACleanup();
+        return 1;
+    }
+
+    // Привязка к локальному адресу
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(port);
+    if (bind(sock, (sockaddr*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed. Error code: " << WSAGetLastError() << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server started. Waiting for messages on port " << port << "...\n";
+
+    // Главный цикл: прием и отправка сообщений
+    while (true) {
+        int recvlen = recvfrom(sock, recvbuf, sizeof(recvbuf) - 1, 0, (sockaddr*)&clientAddr, &clientAddrLen);
+        if (recvlen == SOCKET_ERROR) {
+            std::cerr << "Error receiving data. Error code: " << WSAGetLastError() << std::endl;
+            continue;
+        }
+        recvbuf[recvlen] = '\0';
+
+        std::cout << "Received from client: " << recvbuf << std::endl;
+
+        // Отправка обратно клиенту
+        int sentlen = sendto(sock, recvbuf, recvlen, 0, (sockaddr*)&clientAddr, clientAddrLen);
+        if (sentlen == SOCKET_ERROR) {
+            std::cerr << "Error sending data. Error code: " << WSAGetLastError() << std::endl;
+        }
+
+        // Если строка начинается с 'x', завершить работу
+        if (recvbuf[0] == 'x') break;
+    }
+
+    // Закрытие соединения
+    closesocket(sock);
+    WSACleanup();
+    std::cout << "Server stopped." << std::endl;
+    return 0;
+}
